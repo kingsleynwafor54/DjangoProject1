b0db417d2db5aaf41d004a3883b3f92da1f036ab
Kingsley Nwafor <kingsleynwafor54@gmail.com>
2021-09-22 22:13:29 +0100

    djangoAlbum

--- music/models.py
+++ music/models.py
@@ -9,7 +9,12 @@ class Album(models.Model):
     genre=models.CharField(max_length=100)
     album_logo=models.CharField(max_length=1000)
 
+    def __str__(self):
+        return self.album+"-"+self.artist
+
+
+
 class Song(models.Model):
-    album =models.ForeignKey(Album,on_delete=models.CASCADE)
+    album=models.ForeignKey(Album,on_delete=models.CASCADE)
     file_type=models.CharField(max_length=10)
     song_title=models.CharField(max_length=250)
--- music/urls.py
+++ music/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
-from django.shortcuts import render
 from .import views
 # create your views here
 
 
 urlpatterns = [
+    #/music/
+      url(r'^$', views.index,name='index'),
+    #/music/712/
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail,name='detail' ),
 
-    url(r'^$', views.index,name='index'),
 ]
